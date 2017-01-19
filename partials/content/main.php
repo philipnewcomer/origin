cf15206f98dfd7061b6d2b27e5996a680f667968
Philip Newcomer <contact@philipnewcomer.net>
2017-01-19 09:50:45 -0500

    Make markup in content/main partial consistent with the rest of the project

--- partials/content/main.php
+++ partials/content/main.php
@@ -1,18 +1,19 @@
 <div class="content-main">
 	<?php
-	if ( is_singular() ) :
+	if ( is_singular() ) : ?>
 
-		the_content();
+		<?php the_content(); ?>
 
+		<?php
 		wp_link_pages( [
 			'before' => '<div class="content-page-links">' . esc_html__( 'Pages:', 'origin' ),
 			'after'  => '</div>',
 		] );
+		?>
 
-	else :
+	<?php else : ?>
 
-		the_excerpt();
+		<?php the_excerpt(); ?>
 
-	endif;
-	?>
+	<?php endif; ?>
 </div>
